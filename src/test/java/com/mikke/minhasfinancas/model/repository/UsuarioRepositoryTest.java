d83a612cf438a5c2effd5f42932a5ba5e37804da
mikkenienow <34070014+mikkenienow@users.noreply.github.com>
2023-07-03 15:26:30 -0300

    Aula 35

--- src/test/java/com/mikke/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/mikke/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.mikke.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
@@ -17,11 +19,27 @@ public class UsuarioRepositoryTest {
 	
 	@Test
 	public void deveLancarErroAoValidarEmailJaCadastrado() {
-
-			Usuario usuario = Usuario.builder().nome("usuario").email("Usuario@email.com").senha("123456").build();
-			repository.save(usuario);
-			boolean result = repository.existsByEmail("Usuario@email.com");
-			Assertions.assertThat(result).isTrue();
+		//cenario
+		Usuario usuario = Usuario.builder().nome("usuario").email("Usuario@email.com").senha("123456").build();
+		repository.save(usuario);
+		
+		//ação / execução
+		boolean result = repository.existsByEmail("Usuario@email.com");
+		
+		//verificação
+		Assertions.assertThat(result).isTrue();
 			
+	}
+	
+	@Test
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail(){
+		//cenario
+		repository.deleteAll();
+		
+		//acao
+		boolean result = repository.existsByEmail("usuario@email.com");
+		
+		//verificacao
+		Assertions.assertThat(result).isFalse();
 	}
 }
